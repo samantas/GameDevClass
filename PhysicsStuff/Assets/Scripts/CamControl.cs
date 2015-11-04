d3ef2ad2e72422bf536cb3c21b96a67f69956c98
Samanta <samantas@nyu.edu>
2015-11-04 18:02:20 -0500

    Dominos Project
    Initiated HW Assignment due next week.

---
+++ PhysicsStuff/Assets/Scripts/CamControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+	public GameObject[] cams;
+	private int currentCam = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		ActivateCam (currentCam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			currentCam += 1;
+
+			if(currentCam == cams.Length){
+				currentCam = 0;
+			}
+
+			ActivateCam (currentCam);
+		}
+
+		}
+
+	public void DeactivateAllCams(){
+		for (int i = 0; i < cams.Length; i++) {
+			//equivalent of something.enabled = false; for components but for game objects! 
+			cams[i].SetActive (false);
+		}
+	}
+
+	public void ActivateCam(int camIndex){
+
+		DeactivateAllCams ();
+		//activate camindex cam
+		cams [camIndex].SetActive (true);
+	}
+		
+}
---
+++ PhysicsStuff/Assets/Scripts/CamTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamTrigger : MonoBehaviour {
+
+	public CamControl camControl;
+	public GameObject camToActivate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider myTrigger){
+		
+		Debug.Log ("Trigger hit!");
+
+		camControl.DeactivateAllCams ();
+
+		//activate camToActivate
+		camToActivate.SetActive (true);
+
+	}
+}
