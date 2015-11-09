81a13b95b7f88dd15f1cf592bfa2c40709be1f1d
Samanta <samantas@nyu.edu>
2015-11-09 18:41:49 -0500

    started some updates with regards to UI Text

---
+++ PhysicsStuff/Assets/Scripts/InteractiveText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InteractiveText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		string updateText = "";
+
+		GetComponent<Text> ().text = updateText;
+
+	}
+}
